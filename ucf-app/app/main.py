536928bf51802bf195bebafd17220c11ac1a3113
Ilya Beda <ir4y.ix@gmail.com>
2024-11-12 12:34:03 +1100

    Sending email after completing a questionnaire
    Ref https://github.com/HadleyLab/ucfwealth/issues/34

--- ucf-app/app/main.py
+++ ucf-app/app/main.py
@@ -11,7 +11,7 @@ import logging
 
 
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.DEBUG
 )
 
 async def application() -> web.Application:
